a2844b36d2bae1a0042dd6233c1624579459b718
positive-season <scp94373@gmail.com>
2024-03-30 21:52:37 +0000

    Posts questions and rerenders the page by updating the array after there is a response from POST

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,26 @@ const App: FC = () => {
       {id: 0, answered: true, question: "What is my question?", answer: "No idea!", dateCreated: "2024/03/28", dateUpdated: "2024/03/28"},
       {id: 1, answered: false, question: "Is this empty?", answer: "", dateCreated: "2024/03/28", dateUpdated: ""},
   ]
+  const shiftKeyCode = 16;
+  const enterKeyCode = 13;
 
   const [questions, setQuestions] = useState<QuestionObj[]>();
   const [currentQuestion, setCurrentQuestion] = useState<string | number | readonly string[] | undefined>("");
+  const [shiftKeyHeldDown, setShiftKeyHeldDown] = useState<boolean>(false);
 
 
-  const submitHandler = () => {
-    console.log("Submit my question");
+  const submitQuestion = () => {
     const data = {
       answered: false,
       question: currentQuestion,
       answer: "",
       type: "General"
     }
-    console.log("Sent to Server: ", data);
-    postData("http://localhost:5125/questions", data);
-
-  }
-
-  const deleteDatabaseHandler = () => {
-    console.log("Cleared all items in DB");
+    postData("http://localhost:5125/questions", data)
+      .then(() => {
+        populateQuestions();
+        setCurrentQuestion("");
+      });
   }
 
   const setMockData = () => {
@@ -45,7 +45,7 @@ const App: FC = () => {
     setQuestions(data); //Returns an array of all forecasts
   }
 
-  const   postData = async (url = "", data = {}) => {
+  const postData = async (url = "", data = {}) => {
     const response = await fetch(url, {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
@@ -62,6 +62,17 @@ const App: FC = () => {
     return response.json(); // parses JSON response into native JavaScript objects
   }
 
+  const onHandleKeyDown = (event: { keyCode: number; }) => {
+    console.log(event);
+    if(event.keyCode === shiftKeyCode) setShiftKeyHeldDown(true);
+    if(shiftKeyHeldDown && event.keyCode === enterKeyCode) submitQuestion();
+  }
+
+  const onHandleKeyUp = (event: { keyCode: number; }) => {
+    console.log(event);
+    if(event.keyCode === shiftKeyCode) setShiftKeyHeldDown(false); 
+  }
+
   useEffect(() => {
 
     // setMockData();
@@ -74,7 +85,7 @@ const App: FC = () => {
     <>
       <div id="askSamContainer" className="text-center py-12 bg-slate-200 text-slate-600">
           <h1 className="text-4xl font-semibold ">{header}</h1>
-          <div className="bg-white rounded-xl mx-auto m-6 shadow-md max-w-3xl">
+          <div className="flex bg-white rounded-xl mx-auto m-6 shadow-md max-w-3xl">
               
               <textarea v-model="question"
                   className=" bg-slate-50 rounded-xl shadow-md shadow-teal-200 p-3 w-full text-center focus:border-2 focus:border-teal-300 focus:outline-none"
@@ -83,19 +94,17 @@ const App: FC = () => {
                   onChange={(event) => {
                     setCurrentQuestion(event.target.value);
                   }}
+                  onKeyDown={onHandleKeyDown}
+                  onKeyUp={onHandleKeyUp}
                   ></textarea>
-              <div className="flex place-content-center h-16 ">
+              <div className="">
                   <button 
-                      className="w-32 h-16 rounded border-2 border-teal-200 transition ease-in-out hover:border-white hover:bg-teal-200 duration-300 px-7 focus:bg-teal-200"
-                      onClick={submitHandler}
+                      className="w-32 h-full rounded border-2 border-teal-200 transition ease-in-out hover:border-white hover:bg-teal-200 duration-300 px-7 focus:bg-teal-200"
+                      onClick={submitQuestion}
                       >Submit</button>
-                      
-                  <button 
-                      className="w-32 rounded border-2 border-red-200 transition ease-in-out hover:border-white hover:bg-red-200 duration-300 px-7 focus:bg-red-200"
-                      onClick={deleteDatabaseHandler}
-                      >Clear Database</button>
               </div>    
           </div>
+          <span className="text-gray-400 italic">Desktop: Shift + Enter to Submit</span>
           <div>
               <h1 className="font-semibold text-2xl p-3">Question History</h1>
               <ul className="bg-white shadow-md rounded-xl mx-auto max-w-lg">
